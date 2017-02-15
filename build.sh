af227f6f19c2780db53ab5d766f7603da027e781
Peter Idah <peter.idah@pearson.com>
2017-02-15 16:58:47 +0000

    add files

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-VERSION=${1:-0.0.13}
-DOCKER_REPO="pidah/test-master"
+VERSION=${1:-0.0.12}
+DOCKER_REPO="pidah/test-tpr-s3"
 DOCKER_IMAGE=${DOCKER_REPO}:${VERSION}
 
 # run tests before build
@@ -11,9 +11,9 @@ DOCKER_IMAGE=${DOCKER_REPO}:${VERSION}
 #go test
 
 echo "Building application..."
-git tag ${VERSION}
-git push origin ${VERSION}
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.8 bash -c "go get -d && CGO_ENABLED=0 GOOS=linux go build -tags netgo -installsuffix netgo -o test-master -ldflags '-w' -a -v"
+#git tag ${VERSION}
+#git push origin ${VERSION}
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.8 bash -c "go get -d && CGO_ENABLED=0 GOOS=linux go build -tags netgo -installsuffix netgo -o test-tpr-s3 -ldflags '-w' -a -v"
 
 echo "Building docker image..."
 docker build -t ${DOCKER_IMAGE} .
